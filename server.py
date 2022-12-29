12fe488891468745ecd64d7f8d7da2919ebaddbe
manuelchavestirapegui <manuchaves95@hotmail.com>
2022-12-29 16:01:34 +0100

    Creation of database for messages.

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
 import csv
-
+from flask_bootstrap import Bootstrap
 from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-print(__name__)
 
+app.config['SECRET_KEY'] = "x98\xefpQ\xe6\xf6\xff\xac\xd4\xa0\x17\x12"
+Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+all_messages = []
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    message = db.Column(db.String(1000), nullable=False)
+"""
+with app.app_context():
+    db.create_all()
+"""
 
 @app.route('/')
 def my_home():
@@ -30,13 +48,26 @@ def write_to_csv(data):
         email = data['email']
         message = data['message']
         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([name,email,message])
+        csv_writer.writerow([name, email, message])
+
+
+def add_to_database(data):
+    with app.app_context():
+        new_message = Message(
+            name=data['name'],
+            email=data['email'],
+            message=data['message']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
         write_to_csv(data)
+        add_to_database(data)
         return redirect('thankyou.html')
     else:
         return 'Something went wrong, please, try again.'
