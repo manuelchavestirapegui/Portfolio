edf9457b729fdac06771bd3e54ef186b8793e62d
manuelchavestirapegui <manuchaves95@hotmail.com>
2022-12-30 10:59:27 +0100

    Requirements update

--- server.py
+++ server.py
@@ -1,27 +1,9 @@
 import csv
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__)
+print(__name__)
 
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-all_messages = []
-
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    email = db.Column(db.String(250), nullable=False)
-    message = db.Column(db.String(1000), nullable=False)
-"""
-with app.app_context():
-    db.create_all()
-"""
 
 @app.route('/')
 def my_home():
@@ -33,40 +15,19 @@ def html_page(page_name):
     return render_template(page_name)
 
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        name = data['name']
-        email = data['email']
-        message = data['message']
-        file = database.write(f'\n{name},{email},{message}')
-
-
 def write_to_csv(data):
     with open('database.csv', newline='', mode='a') as database2:
         name = data['name']
         email = data['email']
         message = data['message']
         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([name, email, message])
-
-
-def add_to_database(data):
-    with app.app_context():
-        new_message = Message(
-            name=data['name'],
-            email=data['email'],
-            message=data['message']
-        )
-        db.session.add(new_message)
-        db.session.commit()
-
+        csv_writer.writerow([name,email,message])
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
         write_to_csv(data)
-        add_to_database(data)
         return redirect('thankyou.html')
     else:
-        return 'Something went wrong, please try again.'
+        return 'Something went wrong, please, try again.'
